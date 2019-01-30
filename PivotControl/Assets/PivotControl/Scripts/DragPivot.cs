206f54986328401ff2806e13420ecf5523411b86
晃弘 上山 <akihiro@hololab.co.jp>
2019-01-30 20:51:17 +0900

    first commit パッケージ化

---
+++ PivotControl/Assets/PivotControl/Scripts/DragPivot.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HoloLensModule.Input;
+
+namespace PivotControl
+{
+    /// <summary>
+    /// 選択Pivotを通知し，Y軸回転をドラッグで行う
+    /// </summary>
+    public class DragPivot : MonoBehaviour,IDragGestureInterface
+    {
+        /// <summary>
+        /// Pivot選択イベント
+        /// </summary>
+        public event Action<Transform> OnClickPivot;
+
+        private Vector3 current;
+
+        public void OnStartDrag(Vector3 pos, Quaternion? rot = null)
+        {
+            current = pos;
+        }
+
+        public void OnUpdateDrag(Vector3 pos, Quaternion? rot = null)
+        {
+            if (OnClickPivot != null) OnClickPivot.Invoke(transform);
+            transform.Rotate(0, (pos.x - current.x) * 100.0f, 0);
+            current = pos;
+        }
+    }
+}
---
+++ PivotControl/Assets/PivotControl/Scripts/PivotController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PivotControl
+{
+    /// <summary>
+    /// 選択Pivotとの親子関係を変更することでPivot中心の回転が行える
+    /// </summary>
+    public class PivotController : MonoBehaviour
+    {
+        /// <summary>
+        /// 操作対象オブジェクト
+        /// </summary>
+        [SerializeField] private Transform target;
+        [Space(14)]
+        /// <summary>
+        /// 操作対象オブジェクトを格納するオブジェクト
+        /// </summary>
+        [SerializeField] private Transform root;
+        /// <summary>
+        /// Pivotオブジェクト
+        /// </summary>
+        [SerializeField] private DragPivot[] pivots;
+
+        // Use this for initialization
+        void Start()
+        {
+            if (target != null) SetTargetObject(target);
+            foreach (var p in pivots) p.OnClickPivot += t =>
+            {
+                foreach (var pivot in pivots) pivot.transform.parent = transform;
+                t.parent = null;
+                transform.parent = t;
+            };
+        }
+
+        public void SetTargetObject(Transform t)
+        {
+            t.parent = root;
+            t.localPosition = Vector3.zero; ;
+            t.localRotation = Quaternion.identity;
+        }
+    }
+}
